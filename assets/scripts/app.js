435bd60ad5f94b8fbb9d83482aca5f92d94768b8
chrisBouwer <chrisbouwer01@gmail.com>
2021-09-28 11:23:43 +0200

    Added calc logic into app.js

---
+++ assets/scripts/app.js
@@ -0,0 +1,19 @@
+//add the calc logic
+
+//alert('This works');
+
+const defaultResult = 1;
+
+let currentResult = defaultResult;
+
+currentResult += 10;
+
+//alert('Current Result:' + currentResult);
+
+outputResult(currentResult, `default restult = ${defaultResult}`);
+
+// template literal:  `${var}`
+
+let errorMessage = 'An error \n n \t tab occurred!';
+
+console.log(errorMessage);
